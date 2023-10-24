020149db0ea83dd283747d695672dbc4dd36eb78
Kirikllz909 <kirikllz909@gmail.com>
2023-10-24 13:03:04 +0300

    Added new const (button text and ev, ghost lists)

--- src/main/java/com/phasmoghostbot/telegrambot/constants/Constants.java
+++ src/main/java/com/phasmoghostbot/telegrambot/constants/Constants.java
@@ -1,6 +1,17 @@
 package com.phasmoghostbot.telegrambot.constants;
 
+import java.util.List;
+
+import com.phasmoghostbot.telegrambot.api.dataLoader.GenerateXMLFilePath;
+import com.phasmoghostbot.telegrambot.impl.dataLoader.EvidenceLoader;
+import com.phasmoghostbot.telegrambot.impl.dataLoader.GhostLoader;
+import com.phasmoghostbot.telegrambot.models.Evidence;
+import com.phasmoghostbot.telegrambot.models.Ghost;
+
 public interface Constants {
+    List<Ghost> GHOST_LIST = new GhostLoader().load(GenerateXMLFilePath.getFileLocation());
+    List<Evidence> EVIDENCE_LIST = new EvidenceLoader().load(GenerateXMLFilePath.getFileLocation());
+
     String CHAT_SEARCH_PARAMS = "GHOST_SEARCH_PARAMS";
     String CHAT_STATE = "CHAT_STATE";
 
@@ -8,5 +19,9 @@ public interface Constants {
 
     String SELECT_MODE_MESSAGE = "Select mode";
     String SELECT_MODE_BUTTON_INFORMATION = "Information";
+    String SELECT_WHICH_MODE_INFORMATION_MESSAGE = "Select what information do you need";
+    String SELECTED_MODE_BUTTON_INFORMATION_EVIDENCE = "Evidence";
+    String SELECTED_MODE_BUTTON_INFORMATION_GHOST = "Ghost";
+
     String SELECT_MODE_BUTTON_GHOST_SOLVER = "Ghost solver";
 }
