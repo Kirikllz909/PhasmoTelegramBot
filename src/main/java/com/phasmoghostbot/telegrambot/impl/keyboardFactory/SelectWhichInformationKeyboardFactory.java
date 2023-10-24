bfd2d48551214514c287e33024f3ed248ee0053a
Kirikllz909 <kirikllz909@gmail.com>
2023-10-24 13:02:31 +0300

    Created new keyboard for selecting which info need

---
+++ src/main/java/com/phasmoghostbot/telegrambot/impl/keyboardFactory/SelectWhichInformationKeyboardFactory.java
@@ -0,0 +1,38 @@
+package com.phasmoghostbot.telegrambot.impl.keyboardFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import com.phasmoghostbot.telegrambot.api.keyboardFactory.KeyboardFactory;
+import com.phasmoghostbot.telegrambot.constants.Constants;
+
+public class SelectWhichInformationKeyboardFactory implements KeyboardFactory {
+
+    @Override
+    public ReplyKeyboard generateKeyboard() {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        List<InlineKeyboardButton> buttonsInRows = new ArrayList<>();
+
+        InlineKeyboardButton buttonEvidence = new InlineKeyboardButton();
+        buttonEvidence.setText(Constants.SELECT_MODE_BUTTON_INFORMATION);
+        buttonEvidence.setCallbackData(Constants.SELECT_MODE_BUTTON_INFORMATION);
+
+        InlineKeyboardButton buttonGhost = new InlineKeyboardButton();
+        buttonGhost.setText(Constants.SELECTED_MODE_BUTTON_INFORMATION_GHOST);
+        buttonGhost.setCallbackData(Constants.SELECTED_MODE_BUTTON_INFORMATION_GHOST);
+
+        buttonsInRows.add(buttonEvidence);
+        buttonsInRows.add(buttonGhost);
+        rows.add(buttonsInRows);
+
+        markup.setKeyboard(rows);
+
+        return markup;
+    }
+
+}
