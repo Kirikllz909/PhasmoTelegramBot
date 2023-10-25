9cda7174a917394b17a551010fee535650b86734
Kirikllz909 <kirikllz909@gmail.com>
2023-10-25 18:40:27 +0300

    Added new consts for info mod

--- src/main/java/com/phasmoghostbot/telegrambot/constants/Constants.java
+++ src/main/java/com/phasmoghostbot/telegrambot/constants/Constants.java
@@ -12,16 +12,25 @@ public interface Constants {
     List<Ghost> GHOST_LIST = new GhostLoader().load(GenerateXMLFilePath.getFileLocation());
     List<Evidence> EVIDENCE_LIST = new EvidenceLoader().load(GenerateXMLFilePath.getFileLocation());
 
-    String CHAT_SEARCH_PARAMS = "GHOST_SEARCH_PARAMS";
-    String CHAT_STATE = "CHAT_STATE";
+    String BACK_BUTTON_TEXT = "<< Back";
 
+    String CHAT_SEARCH_PARAMS = "GHOST_SEARCH_PARAMS";
+
+    String START_MODE = "START";
     String START_DESCRIPTION = "Start ghost hunting or check the information about each ghost or evidence";
 
     String SELECT_MODE_MESSAGE = "Select mode";
+
     String SELECT_MODE_BUTTON_INFORMATION = "Information";
     String SELECT_WHICH_MODE_INFORMATION_MESSAGE = "Select what information do you need";
-    String SELECTED_MODE_BUTTON_INFORMATION_EVIDENCE = "Evidence";
-    String SELECTED_MODE_BUTTON_INFORMATION_GHOST = "Ghost";
 
-    String SELECT_MODE_BUTTON_GHOST_SOLVER = "Ghost solver";
+    String SELECTED_MODE_BUTTON_INFORMATION_EVIDENCE = "Evidence";
+    String SELECTED_MODE_BUTTON_INFORMATION_EVIDENCE_MESSAGE = "Here you can check information about any evidence";
+    String SELECTED_EVIDENCE = "Selected_evidence";
+
+    String SELECTED_MODE_BUTTON_INFORMATION_GHOST = "Ghost";
+    String SELECTED_MODE_BUTTON_INFORMATION_GHOST_MESSAGE = "Here you can check information about any ghost";
+    String SELECTED_GHOST = "Selected_ghost";
+
+    String SELECT_MODE_BUTTON_GHOST_SOLVER = "Ghost_solver";
 }
