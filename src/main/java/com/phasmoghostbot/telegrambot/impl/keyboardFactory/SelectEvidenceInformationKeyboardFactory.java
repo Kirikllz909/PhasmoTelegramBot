03a98b059cd19ada31363ccdf9f310b4ae332ef4
Kirikllz909 <kirikllz909@gmail.com>
2023-10-26 02:39:08 +0300

    Update keyboardmarkup for any evidence count

--- src/main/java/com/phasmoghostbot/telegrambot/impl/keyboardFactory/SelectEvidenceInformationKeyboardFactory.java
+++ src/main/java/com/phasmoghostbot/telegrambot/impl/keyboardFactory/SelectEvidenceInformationKeyboardFactory.java
@@ -18,7 +18,16 @@ public class SelectEvidenceInformationKeyboardFactory implements KeyboardFactory
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
         List<List<InlineKeyboardButton>> rows = new ArrayList<>();
 
-        for (int i = 0; i < Constants.EVIDENCE_LIST.size() - 1; i += 2) {
+        boolean isEvidenceCountEven = false;
+        int arrIterationCount;
+
+        if (Constants.EVIDENCE_LIST.size() % 2 == 0) {
+            isEvidenceCountEven = true;
+            arrIterationCount = Constants.EVIDENCE_LIST.size();
+        } else
+            arrIterationCount = Constants.EVIDENCE_LIST.size() - 1;
+
+        for (int i = 0; i < arrIterationCount; i += 2) {
             Evidence currentEvidence = Constants.EVIDENCE_LIST.get(i);
             Evidence nextEvidence = Constants.EVIDENCE_LIST.get(i + 1);
 
@@ -32,17 +41,22 @@ public class SelectEvidenceInformationKeyboardFactory implements KeyboardFactory
 
             rows.add(List.of(evidence1, evidence2));
         }
-        Evidence lastEvidence = Constants.EVIDENCE_LIST.get(Constants.EVIDENCE_LIST.size() - 1);
-
-        InlineKeyboardButton lastEvidenceButton = new InlineKeyboardButton();
-        lastEvidenceButton.setText(lastEvidence.getName());
-        lastEvidenceButton.setCallbackData(Constants.SELECTED_EVIDENCE + " " + lastEvidence.getId());
 
         InlineKeyboardButton backButton = new InlineKeyboardButton();
         backButton.setText(Constants.BACK_BUTTON_TEXT);
         backButton.setCallbackData(Constants.SELECT_MODE_BUTTON_INFORMATION);
 
-        rows.add(List.of(lastEvidenceButton, backButton));
+        if (isEvidenceCountEven == false) {
+            Evidence lastEvidence = Constants.EVIDENCE_LIST.get(Constants.EVIDENCE_LIST.size() - 1);
+
+            InlineKeyboardButton lastEvidenceButton = new InlineKeyboardButton();
+            lastEvidenceButton.setText(lastEvidence.getName());
+            lastEvidenceButton.setCallbackData(Constants.SELECTED_EVIDENCE + " " + lastEvidence.getId());
+
+            rows.add(List.of(lastEvidenceButton, backButton));
+        } else
+            rows.add(List.of(backButton));
+
         markup.setKeyboard(rows);
 
         return markup;
