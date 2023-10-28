94f4032245f50a17a554e001a9a947d3836d777f
Kirikllz909 <kirikllz909@gmail.com>
2023-10-28 23:05:23 +0300

    Created method for creating button

--- src/main/java/com/phasmoghostbot/telegrambot/api/keyboardFactory/KeyboardFactory.java
+++ src/main/java/com/phasmoghostbot/telegrambot/api/keyboardFactory/KeyboardFactory.java
@@ -1,7 +1,15 @@
 package com.phasmoghostbot.telegrambot.api.keyboardFactory;
 
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 public interface KeyboardFactory {
     public ReplyKeyboard generateKeyboard();
+
+    default InlineKeyboardButton generateKeyboardButton(String buttonText, String callbackData) {
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText(buttonText);
+        button.setCallbackData(callbackData);
+        return button;
+    }
 }
