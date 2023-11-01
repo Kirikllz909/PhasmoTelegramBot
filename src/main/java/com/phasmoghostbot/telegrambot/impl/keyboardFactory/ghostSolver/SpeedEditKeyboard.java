9b3e5c960546e3bb48686f0367331496f86dbc5f
Kirikllz909 <kirikllz909@gmail.com>
2023-11-01 21:36:32 +0300

    added keyboard for editing speed

---
+++ src/main/java/com/phasmoghostbot/telegrambot/impl/keyboardFactory/ghostSolver/SpeedEditKeyboard.java
@@ -0,0 +1,34 @@
+package com.phasmoghostbot.telegrambot.impl.keyboardFactory.ghostSolver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import com.phasmoghostbot.telegrambot.api.keyboardFactory.KeyboardFactory;
+import com.phasmoghostbot.telegrambot.constants.Constants;
+
+public class SpeedEditKeyboard implements KeyboardFactory {
+
+    @Override
+    public ReplyKeyboard generateKeyboard() {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+
+        InlineKeyboardButton defaultSpeedButton = generateKeyboardButton("Default",
+                Constants.GHOST_SOLVER_SET_SPEED_ACTION_CALLBACK + "Default");
+        InlineKeyboardButton shiftingSpeedButton = generateKeyboardButton("Shifting",
+                Constants.GHOST_SOLVER_SET_SPEED_ACTION_CALLBACK + "Shifting");
+        InlineKeyboardButton backButton = generateKeyboardButton(Constants.BACK_BUTTON_TEXT,
+                Constants.GHOST_SOLVER_CALLBACK);
+
+        rows.add(List.of(defaultSpeedButton, shiftingSpeedButton));
+        rows.add(List.of(backButton));
+
+        markup.setKeyboard(rows);
+
+        return markup;
+    }
+}
