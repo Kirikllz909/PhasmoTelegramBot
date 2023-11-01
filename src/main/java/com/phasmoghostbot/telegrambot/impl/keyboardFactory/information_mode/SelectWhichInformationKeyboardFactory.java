5e5d8a2b948eb97eedea4208ef8f2fe30fd7088a
Kirikllz909 <kirikllz909@gmail.com>
2023-11-01 21:34:45 +0300

    Fixed wrong margin

--- src/main/java/com/phasmoghostbot/telegrambot/impl/keyboardFactory/information_mode/SelectWhichInformationKeyboardFactory.java
+++ src/main/java/com/phasmoghostbot/telegrambot/impl/keyboardFactory/information_mode/SelectWhichInformationKeyboardFactory.java
@@ -12,31 +12,31 @@ import com.phasmoghostbot.telegrambot.constants.Constants;
 
 public class SelectWhichInformationKeyboardFactory implements KeyboardFactory {
 
-        @Override
-        public ReplyKeyboard generateKeyboard() {
-                InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-                List<List<InlineKeyboardButton>> rows = new ArrayList<>();
-                List<InlineKeyboardButton> buttonsInRows = new ArrayList<>();
+    @Override
+    public ReplyKeyboard generateKeyboard() {
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+        List<InlineKeyboardButton> buttonsInRows = new ArrayList<>();
 
-                InlineKeyboardButton buttonEvidence = this.generateKeyboardButton(
-                                Constants.INFORMATION_MODE_EVIDENCE_CALLBACK,
-                                Constants.INFORMATION_MODE_EVIDENCE_CALLBACK);
+        InlineKeyboardButton buttonEvidence = this.generateKeyboardButton(
+                Constants.INFORMATION_MODE_EVIDENCE_CALLBACK,
+                Constants.INFORMATION_MODE_EVIDENCE_CALLBACK);
 
-                InlineKeyboardButton buttonGhost = this.generateKeyboardButton(
-                                Constants.INFORMATION_MODE_GHOST_CALLBACK,
-                                Constants.INFORMATION_MODE_GHOST_CALLBACK);
+        InlineKeyboardButton buttonGhost = this.generateKeyboardButton(
+                Constants.INFORMATION_MODE_GHOST_CALLBACK,
+                Constants.INFORMATION_MODE_GHOST_CALLBACK);
 
-                InlineKeyboardButton buttonBackToSelectMode = this.generateKeyboardButton(Constants.BACK_BUTTON_TEXT,
-                                Constants.START_MODE);
+        InlineKeyboardButton buttonBackToSelectMode = this.generateKeyboardButton(Constants.BACK_BUTTON_TEXT,
+                Constants.START_MODE);
 
-                buttonsInRows.add(buttonEvidence);
-                buttonsInRows.add(buttonGhost);
-                rows.add(buttonsInRows);
-                rows.add(List.of(buttonBackToSelectMode));
+        buttonsInRows.add(buttonEvidence);
+        buttonsInRows.add(buttonGhost);
+        rows.add(buttonsInRows);
+        rows.add(List.of(buttonBackToSelectMode));
 
-                markup.setKeyboard(rows);
+        markup.setKeyboard(rows);
 
-                return markup;
-        }
+        return markup;
+    }
 
 }
