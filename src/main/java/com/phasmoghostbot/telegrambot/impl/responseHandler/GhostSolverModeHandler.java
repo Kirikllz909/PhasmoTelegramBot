a8be05d575581372140f2e930d75c3979dc5b24f
Kirikllz909 <kirikllz909@gmail.com>
2023-11-01 22:42:51 +0300

    Added handling of speed changing

--- src/main/java/com/phasmoghostbot/telegrambot/impl/responseHandler/GhostSolverModeHandler.java
+++ src/main/java/com/phasmoghostbot/telegrambot/impl/responseHandler/GhostSolverModeHandler.java
@@ -7,6 +7,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 
 import com.phasmoghostbot.telegrambot.constants.Constants;
 import com.phasmoghostbot.telegrambot.impl.keyboardFactory.ghostSolver.GhostSolverActionsKeyboard;
+import com.phasmoghostbot.telegrambot.impl.keyboardFactory.ghostSolver.SpeedEditKeyboard;
 import com.phasmoghostbot.telegrambot.models.Evidence;
 import com.phasmoghostbot.telegrambot.models.GhostSearchParameters;
 
@@ -25,7 +26,7 @@ public class GhostSolverModeHandler {
         messageText.append(Constants.GHOST_SOLVER_MESSAGE + "\n");
         messageText.append("Current sanity = " + parameters.getCurrentSanity() + "\n");
         messageText.append("Speed = " + parameters.getSpeed() + "\n");
-        messageText.append("\nEvidences =  + \n");
+        messageText.append("\nEvidences:  + \n");
         if (parameters.getEvidences().size() > 0)
             for (Evidence evidence : parameters.getEvidences()) {
                 messageText.append(evidence.getName() + "\n");
@@ -38,5 +39,26 @@ public class GhostSolverModeHandler {
         message.setReplyMarkup(new GhostSolverActionsKeyboard().generateKeyboard());
 
         sender.execute(message);
+    }
+
+    public static void replyToSetSpeedMode(SilentSender sender, long chatId,
+            Map<Long, GhostSearchParameters> ghostSearchParameters) {
+        SendMessage message = new SendMessage();
+        GhostSearchParameters parameters = ghostSearchParameters.get(chatId);
+
+        message.setChatId(chatId);
+        message.setText("Current Speed: " + parameters.getSpeed());
+        message.setReplyMarkup(new SpeedEditKeyboard().generateKeyboard());
+
+        sender.execute(message);
+    }
+
+    public static void changeSpeed(long chatId, String newSpeed,
+            Map<Long, GhostSearchParameters> ghostSearchParameters) {
+        GhostSearchParameters parameters = ghostSearchParameters.get(chatId);
+
+        parameters.setSpeed(newSpeed);
+
+        ghostSearchParameters.put(chatId, parameters);
     }
 }
