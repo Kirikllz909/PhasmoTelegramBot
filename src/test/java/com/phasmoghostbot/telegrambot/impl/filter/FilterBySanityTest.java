fe585523667f27e94f45720e3d6ef0f18b87e733
Kirikllz909 <kirikllz909@gmail.com>
2023-11-02 17:44:34 +0300

    Added new test cases

--- src/test/java/com/phasmoghostbot/telegrambot/impl/filter/FilterBySanityTest.java
+++ src/test/java/com/phasmoghostbot/telegrambot/impl/filter/FilterBySanityTest.java
@@ -5,20 +5,42 @@ import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.phasmoghostbot.telegrambot.api.filter.exceptions.SanityValueIsNegativeException;
 import com.phasmoghostbot.telegrambot.constants.Constants;
 import com.phasmoghostbot.telegrambot.models.Ghost;
 
 public class FilterBySanityTest {
 
-    private final Integer EXPECTED_GHOST_COUNT = 7;
-    private final Integer CURRENT_SANITY = 60;
+    private final Integer EXPECTED_60_SANITY_GHOST_COUNT = 7;
+    private final Integer CURRENT_SANITY_60 = 60;
+
+    private final Integer EXPECTED_ZERO_SANITY_GHOST_COUNT = 24;
+    private final Integer CURRENT_SANITY_0 = 0;
+
+    private final Integer NEGATIVE_SANITY = -1;
 
     @Test
     void testFilter() {
-        FilterBySanity filter = new FilterBySanity(CURRENT_SANITY);
+        FilterBySanity filter = new FilterBySanity(CURRENT_SANITY_60);
 
         List<Ghost> filteredGhosts = filter.filter(Constants.GHOST_LIST);
 
-        Assertions.assertEquals(EXPECTED_GHOST_COUNT, filteredGhosts.size());
+        Assertions.assertEquals(EXPECTED_60_SANITY_GHOST_COUNT, filteredGhosts.size());
+    }
+
+    @Test
+    void testFilterZeroSanity() {
+        FilterBySanity filter = new FilterBySanity(CURRENT_SANITY_0);
+
+        List<Ghost> filteredGhosts = filter.filter(Constants.GHOST_LIST);
+
+        Assertions.assertEquals(EXPECTED_ZERO_SANITY_GHOST_COUNT, filteredGhosts.size());
+    }
+
+    @Test
+    void testFilterNegativeSanity() {
+        FilterBySanity filter = new FilterBySanity(NEGATIVE_SANITY);
+
+        Assertions.assertThrows(SanityValueIsNegativeException.class, () -> filter.filter(Constants.GHOST_LIST));
     }
 }
