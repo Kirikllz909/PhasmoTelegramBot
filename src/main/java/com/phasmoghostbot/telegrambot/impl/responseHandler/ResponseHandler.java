426ef6002abf1939c6bae0c6098f34cca035fa52
Kirikllz909 <kirikllz909@gmail.com>
2023-11-09 21:00:53 +0300

    Made variables for action and data in the callback. Fixed bug with incorrect ghost when ghost name is longer than 1 word

--- src/main/java/com/phasmoghostbot/telegrambot/impl/responseHandler/ResponseHandler.java
+++ src/main/java/com/phasmoghostbot/telegrambot/impl/responseHandler/ResponseHandler.java
@@ -65,7 +65,18 @@ public class ResponseHandler {
         long chatId = update.getCallbackQuery().getMessage().getChatId();
         int messageId = firstMessagesId.get(chatId);
         String buttonCallbackData = update.getCallbackQuery().getData();
-        switch (buttonCallbackData.split(" ")[0]) {
+
+        String[] callback = buttonCallbackData.split(" ");
+        String callbackAction = callback[0];
+        String callbackData = "";
+        if (callback.length > 1) {
+            for (int i = 1; i < callback.length; i++) {
+                callbackData += callback[i] + " ";
+            }
+            callbackData = callbackData.trim();
+        }
+
+        switch (callbackAction) {
             case Constants.START_MODE:
                 replyToStart(chatId);
                 break;
@@ -80,10 +91,10 @@ public class ResponseHandler {
                 informationModeHandler.replyToInformationGhostMode(chatId, messageId);
                 break;
             case Constants.INFORMATION_MODE_SELECTED_EVIDENCE_CALLBACK:
-                informationModeHandler.replyToSelectedEvidence(chatId, messageId, buttonCallbackData.split(" ")[1]);
+                informationModeHandler.replyToSelectedEvidence(chatId, messageId, callbackData);
                 break;
             case Constants.INFORMATION_MODE_SELECTED_GHOST_CALLBACK:
-                informationModeHandler.replyToSelectedGhost(chatId, messageId, buttonCallbackData.split(" ")[1]);
+                informationModeHandler.replyToSelectedGhost(chatId, messageId, callbackData);
                 break;
 
             case Constants.GHOST_SOLVER_CALLBACK:
@@ -93,26 +104,26 @@ public class ResponseHandler {
                 ghostSolverModeHandler.replyToSetSpeedMode(chatId, messageId);
                 break;
             case Constants.GHOST_SOLVER_SET_SPEED_ACTION_CALLBACK:
-                ghostSolverModeHandler.replyToChangeSpeedAction(chatId, messageId, buttonCallbackData.split(" ")[1]);
+                ghostSolverModeHandler.replyToChangeSpeedAction(chatId, messageId, callbackData);
                 break;
             case Constants.GHOST_SOLVER_SET_BLINK_FREQUENCY_MODE_CALLBACK:
                 ghostSolverModeHandler.replyToSetBlinkFrequencyMode(chatId, messageId);
                 break;
             case Constants.GHOST_SOLVER_SET_BLINK_FREQUENCY_ACTION_CALLBACK:
                 ghostSolverModeHandler.replyToChangeBlinkFrequencyAction(chatId,
-                        messageId, buttonCallbackData.split(" ")[1]);
+                        messageId, callbackData);
                 break;
             case Constants.GHOST_SOLVER_SET_CURRENT_SANITY_MODE_CALLBACK:
                 ghostSolverModeHandler.replyToSetSanityMode(chatId, messageId);
                 break;
             case Constants.GHOST_SOLVER_SET_CURRENT_SANITY_ACTION_CALLBACK:
-                ghostSolverModeHandler.replyToSetSanityAction(chatId, messageId, buttonCallbackData.split(" ")[1]);
+                ghostSolverModeHandler.replyToSetSanityAction(chatId, messageId, callbackData);
                 break;
             case Constants.GHOST_SOLVER_SET_EVIDENCES_MODE_CALLBACK:
                 ghostSolverModeHandler.replyToSetEvidences(chatId, messageId);
                 break;
             case Constants.GHOST_SOLVER_SET_EVIDENCES_ACTION_CALLBACK:
-                ghostSolverModeHandler.replyToSetEvidenceAction(chatId, messageId, buttonCallbackData.split(" ")[1]);
+                ghostSolverModeHandler.replyToSetEvidenceAction(chatId, messageId, callbackData);
                 break;
             case Constants.GHOST_SOLVER_GET_POSSIBLE_GHOSTS_CALLBACK:
                 ghostSolverModeHandler.replyToGetGhosts(chatId, messageId);
