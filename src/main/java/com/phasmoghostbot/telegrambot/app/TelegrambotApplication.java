ad8aff015c9da2b0992ac646ab86add0d3db9f47
Kirikllz909 <kirikllz909@gmail.com>
2023-11-09 20:50:24 +0300

    Added bean creating

--- src/main/java/com/phasmoghostbot/telegrambot/app/TelegrambotApplication.java
+++ src/main/java/com/phasmoghostbot/telegrambot/app/TelegrambotApplication.java
@@ -1,13 +1,30 @@
 package com.phasmoghostbot.telegrambot.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import com.phasmoghostbot.telegrambot.bot.PhasmoGhostBot;
 
 @SpringBootApplication
+@ComponentScan("com.phasmoghostbot.telegrambot.bot")
 public class TelegrambotApplication {
+	private static final Logger logger = LoggerFactory.getLogger(TelegrambotApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(TelegrambotApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(TelegrambotApplication.class, args);
+		try {
+			TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+			botsApi.registerBot(ctx.getBean("phasmoGhostBot", PhasmoGhostBot.class));
+		} catch (TelegramApiException e) {
+			logger.error(e.getMessage());
+		}
 	}
 
 }
